e5eef54f0e0a8a7006828eee1ec485177140280f
Sarah Tan <sarah@bluestatedigital.com>
2014-02-05 21:40:03 +0000

    terrible attempt at the front-end JS

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,13 @@
 $(document).foundation('topbar');
+
+var trait = function() {
+	$("#editTraitType").on("change", function() {
+		if ($(this).value == "radio" || 
+			$(this).value == "select" || 
+			$(this).value == "checkbox") {
+
+			$(this).append('<input type="text" name="listoption[]" value="" /> <br />
+				<a href="#" class="add-option">+</a>');
+		}
+	});
+}
