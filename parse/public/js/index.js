683feafe636ffa015e5362f610b8051a80a9644c
Sarah Tan <sarah@bluestatedigital.com>
2014-02-08 19:55:17 +0000

    fix bugs with add/remove list-option functionality

--- parse/public/js/index.js
+++ parse/public/js/index.js
@@ -11,9 +11,7 @@ function selectTraitType() {
 		} else if ((traitType == 'radio' || traitType == 'select' || traitType == 'checkbox') && 
 				$(this).next().not('div.list-options').length) {
 			console.log("add html");
-			$(this).after('<div class="list-options"><div class="row-collapse"><input type="text" name="listoption[]" value="" class="list-option" /></div><div class="row"><a href="#" class="add-list-option">+</a></div></div>');
-			$(this).on('click', $(this).siblings('.add-list-option'), addListOption());
-			$(this).on('click', $(this).siblings('.remove-list-option'), removeListOption());
+			$(this).after('<div class="list-options"><div class="row-collapse"><input type="text" name="listoption[]" value="" class="list-option" /><a href="#" class="remove-list-option">x</a></div><div class="row-collapse"><input type="text" name="listoption[]" value="" class="list-option" /><a href="#" class="remove-list-option">x</a></div><div class="row"><a href="#" class="add-list-option">+</a></div></div>');
 		} else if (traitType !== 'radio' && traitType !== 'select' && traitType !== 'checkbox' && 
 				$(this).next('.list-options').filter(':visible').length) {
 			console.log("hide list-options");
@@ -22,33 +20,35 @@ function selectTraitType() {
 	});
 }
 
-function addListOption() {
+function addListOption(elem, e) {
 	console.log("in addListOption()");
-	$('a.add-list-option').click(function(e) {
-		console.log("in addListOption().click");
-		$(this)
-			.parents('.list-options')
-			.find('input.list-option')
-			.filter(function() { return !$(this).next().is('a.remove-list-option') })
-			.after('<a href="#" class="remove-list-option">x</a>');
-		$(this).before('<div class="row-collapse"><input type="text" name="listoption[]" value="" class="list-option" /></div>');
-		e.preventDefault();
-	});
+	/* go through all prior list option inputs and add remove buttons
+	elem
+		.parents('.list-options')
+		.find('input.list-option')
+		.filter(function() { return !$(this).next().is('a.remove-list-option') })
+		.after('<a href="#" class="remove-list-option">x</a>');*/
+	elem.before('<div class="row-collapse"><input type="text" name="listoption[]" value="" class="list-option" /><a href="#" class="remove-list-option">x</a></div>');
+	e.preventDefault();
 }
 
-function removeListOption() {
+function removeListOption(elem, e) {
+	// TODO: check if it is the last one of the set and prevent deletion if so
 	console.log("in removeListOption()");
-	$('a.remove-list-option').click(function(e) {
-		console.log("in addListOption().click");
-		$(this)
-			.parents('.row-collapse')
-			.remove();
-		e.preventDefault();
-	});
+	elem.parent('.row-collapse').remove();
+	e.preventDefault();
 }
 
 $(document).ready( function() {
 	selectTraitType();
-	addListOption();
-	removeListOption();
+	//addListOption();
+	//removeListOption();
+	$('form').on('click', '.add-list-option', function(e) { 
+		var elem = $(this);
+		addListOption(elem, e) 
+	});
+	$('form').on('click', '.remove-list-option', function(e) { 
+		var elem = $(this);
+		removeListOption(elem, e) 
+	});
 });
